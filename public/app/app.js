9fe8c5250f4e7ddbddef454acf0245ac3e1bc4e2
Joabe Costa <joabetc@hotmail.com>
2018-04-16 18:57:43 -0300

    Moving summary logic to the service

--- public/app/app.js
+++ public/app/app.js
@@ -2,16 +2,10 @@ import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { invoiceService as service } from './invoice/service.js';
 
-const sumItems = code => invoices => invoices
-    .$flatMap(invoice => invoice.items)
-    .filter(item => item.id == code)
-    .reduce((total, item) => total + item.value, 0);
-
 document
     .querySelector('#myButton')
     .onclick = () => 
         service
-            .listAll()
-            .then(sumItems('2143'))
+            .sumItems('2143')
             .then(console.log)
             .catch(console.log);
--- public/app/invoice/service.js
+++ public/app/invoice/service.js
@@ -2,8 +2,16 @@ import { handleStatus } from '../utils/promise-helpers.js';
 
 const API = 'http://localhost:3000/notas';
 
+const sumItems = code => invoices => invoices
+    .$flatMap(invoice => invoice.items)
+    .filter(item => item.id == code)
+    .reduce((total, item) => total + item.value, 0);
+
 export const invoiceService = {
     listAll() {
         return fetch(API).then(handleStatus);
+    },
+    sumItems(code) {
+        return this.listAll().then(sumItems(code));
     }
 };
