2210a613ef62de86cff6db2e484c43370972de64
Joabe Costa <joabetc@hotmail.com>
2018-04-16 16:41:11 -0300

    Rewriting code in a more functional approach

--- public/app/app.js
+++ public/app/app.js
@@ -6,10 +6,10 @@ document
     .onclick = () => 
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(invoices => invoices.$flatMap(invoice => invoice.items))
-            .then(log)
-            .then(items => items.filter(item => item.id == '2143'))
-            .then(log)
-            .then(items => items.reduce((total, item) => total + item.value, 0))
+            .then(invoices => invoices
+                .$flatMap(invoice => invoice.items)
+                .filter(item => item.id == '2143')
+                .reduce((total, item) => total + item.value, 0)
+            )
             .then(console.log)
             .catch(console.log);
