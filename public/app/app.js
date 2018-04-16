b11d4333ab7659dfa79ea20ef7e5c61da92c6201
Joabe Costa <joabetc@hotmail.com>
2018-04-16 16:18:57 -0300

    Retrieving data from server and summaring invoices values

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,14 @@ import { handleStatus } from './utils/promise-helpers.js';
 document
     .querySelector('#myButton')
     .onclick = () => 
-        fetch(`http://localhost:3000/notas`)
+        fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(invoices => console.log(invoices))
+            .then(invoices => invoices.reduce((array, invoice) => array.concat(invoice.items), []))
+            .then(items => {
+                console.log(items);
+                return items;
+            })
+            .then(items => items.filter(item => item.id == '2143'))
+            .then(items => items.reduce((total, item) => total + item.value, 0))
+            .then(console.log)
             .catch(console.log);
