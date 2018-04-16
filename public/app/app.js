a0a7a740e3d0d6d5ef04d971a80496029992105c
Joabe Costa <joabetc@hotmail.com>
2018-04-16 15:39:01 -0300

    Checking response status

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,9 @@ document
     .querySelector('#myButton')
     .onclick = () => 
         fetch(`http://localhost:3000/notas`)
-            .then(res => res.json())
+            .then(res => {
+                if (res.ok) return res.json();
+                return Promise.reject(res.statusText);
+            })
             .then(invoices => console.log(invoices))
             .catch(console.log);
