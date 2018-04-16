144e322424062658028f2b0f5028f66d6101257a
Joabe Costa <joabetc@hotmail.com>
2018-04-16 18:45:31 -0300

    Isolating summary logic into a function

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,16 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 
+const sumItems = invoices => invoices
+    .$flatMap(invoice => invoice.items)
+    .filter(item => item.id == '2143')
+    .reduce((total, item) => total + item.value, 0);
+
 document
     .querySelector('#myButton')
     .onclick = () => 
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(invoices => invoices
-                .$flatMap(invoice => invoice.items)
-                .filter(item => item.id == '2143')
-                .reduce((total, item) => total + item.value, 0)
-            )
+            .then(sumItems)
             .then(console.log)
             .catch(console.log);
