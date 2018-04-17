545a037dc2987fea487fc853ff82f9ab4e97614f
Joabe Costa <joabetc@hotmail.com>
2018-04-17 15:07:48 -0300

    Limiting operations with debounceTime function

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,17 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { invoiceService as service } from './invoice/service.js';
-import { takeUntil } from './utils/operators.js';
+import { takeUntil, debounceTime } from './utils/operators.js';
 
-const operation = takeUntil(3, () =>
+const operation1 = takeUntil(3, () =>
     service
         .sumItems('2143')
         .then(console.log)
         .catch(console.log)
     );
 
+const operation2 = debounceTime(500, operation1);
+
 document
     .querySelector('#myButton')
-    .onclick = operation;
+    .onclick = operation2;
