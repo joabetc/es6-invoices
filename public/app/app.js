903abe30acae5f30df75333d268c5d95b32f0992
Joabe Costa <joabetc@hotmail.com>
2018-04-17 15:53:25 -0300

    Applying more function composition

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,18 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { invoiceService as service } from './invoice/service.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
-const action = debounceTime(500, 
-    takeUntil(3, () =>
-        service
-            .sumItems('2143')
-            .then(console.log)
-            .catch(console.log)
-    )
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const action = operations(() =>
+    service
+        .sumItems('2143')
+        .then(console.log)
+        .catch(console.log)
 );
 
 const operation2 = debounceTime(500, operation1);
