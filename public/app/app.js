62ca5173bd210c4337a14ad15b281495eb876d31
Joabe Costa <joabetc@hotmail.com>
2018-04-17 14:29:25 -0300

    Limiting service call

--- public/app/app.js
+++ public/app/app.js
@@ -3,16 +3,13 @@ import './utils/array-helpers.js';
 import { invoiceService as service } from './invoice/service.js';
 import { takeUntil } from './utils/operators.js';
 
-const showMessage = () => console.log('Hi');
-const operation = takeUntil(3, showMessage);
-
-let counter = 10;
-while(counter--) operation();
+const operation = takeUntil(3, () =>
+    service
+        .sumItems('2143')
+        .then(console.log)
+        .catch(console.log)
+    );
 
 document
     .querySelector('#myButton')
-    .onclick = () => 
-        service
-            .sumItems('2143')
-            .then(console.log)
-            .catch(console.log);
+    .onclick = () => operation();
