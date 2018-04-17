9d0ae65778b841005a7f6a2c516b5853c2310f7a
Joabe Costa <joabetc@hotmail.com>
2018-04-17 16:14:29 -0300

    Testing Promise.race

--- public/app/app.js
+++ public/app/app.js
@@ -3,6 +3,16 @@ import './utils/array-helpers.js';
 import { invoiceService as service } from './invoice/service.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
+const p1 = new Promise((resolve, reject) => 
+    setTimeout(() => resolve('Promise 1 finished'), 3000));
+
+const p2 = new Promise((resolve, reject) => 
+    setTimeout(() => resolve('Promise 2 finished'), 1000));
+
+Promise.race([p1, p2])
+    .then(console.log)
+    .catch(console.log);
+
 const operations = pipe(
     partialize(takeUntil, 3),
     partialize(debounceTime, 500)
