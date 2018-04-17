3bb2ffc37de0301bef8342597c2ccc8368e6e472
Joabe Costa <joabetc@hotmail.com>
2018-04-17 16:32:05 -0300

    Implementing promise timeout

--- public/app/app.js
+++ public/app/app.js
@@ -1,17 +1,7 @@
-import { handleStatus, log } from './utils/promise-helpers.js';
+import { log, timeoutPromise } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { invoiceService as service } from './invoice/service.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
-
-const p1 = new Promise((resolve, reject) => 
-    setTimeout(() => resolve('Promise 1 finished'), 2000));
-
-const p2 = new Promise((resolve, reject) => 
-    setTimeout(() => reject('Promise 2 canceled'), 1000));
-
-Promise.race([p1, p2])
-    .then(console.log)
-    .catch(console.log);
 
 const operations = pipe(
     partialize(takeUntil, 3),
@@ -19,13 +9,10 @@ const operations = pipe(
 );
 
 const action = operations(() =>
-    service
-        .sumItems('2143')
+    timeoutPromise(200, service.sumItems('2143'))
         .then(console.log)
         .catch(console.log)
 );
-
-const operation2 = debounceTime(500, operation1);
 
 document
     .querySelector('#myButton')
