402257ecb0b376c35b656ed668b32c5778b6f7cf
Joabe Costa <joabetc@hotmail.com>
2018-04-17 14:26:35 -0300

    Limiting operations with takeUntil just like RxJS

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,13 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { invoiceService as service } from './invoice/service.js';
+import { takeUntil } from './utils/operators.js';
+
+const showMessage = () => console.log('Hi');
+const operation = takeUntil(3, showMessage);
+
+let counter = 10;
+while(counter--) operation();
 
 document
     .querySelector('#myButton')
