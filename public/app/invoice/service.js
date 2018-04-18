a01d63a4191db0ba87b7bbe3c5b98c4b5d0c6d82
Joabe Costa <joabetc@hotmail.com>
2018-04-18 11:39:47 -0300

    Using the monad type

--- public/app/invoice/service.js
+++ public/app/invoice/service.js
@@ -1,16 +1,18 @@
 import { handleStatus } from '../utils/promise-helpers.js';
 import { partialize, pipe } from '../utils/operators.js';
+import { Maybe } from '../utils/maybe.js'
 
 const API = 'http://localhost:3000/notas';
 
-const getItemsFromInvoices = invoices => invoices.$flatMap(invoice => invoice.items);
-const filterItemsByCode = (code, items) => items.filter(item => item.id == code);
-const sumItemsValue = items => items.reduce((total, item) => total + item.value, 0);
+const getItemsFromInvoices = invoicesM => invoicesM.map(invoices => invoices.$flatMap(invoice => invoice.items));
+const filterItemsByCode = (code, itemsM) => itemsM.map(items => items.filter(item => item.id == code));
+const sumItemsValue = itemsM => itemsM.map(items => items.reduce((total, item) => total + item.value, 0));
 
 export const invoiceService = {
     listAll() {
         return fetch(API)
             .then(handleStatus)
+            .then(invoices => Maybe.of(invoices))
             .catch(err => {
                 console.log(err);
                 return Promise.reject('It was not possible to retrieve invoices data');
@@ -26,6 +28,7 @@ export const invoiceService = {
         );
 
         return this.listAll()
-            .then(sumItems);
+            .then(sumItems)
+            .then(result => result.getOrElse(0));
     }
 };
