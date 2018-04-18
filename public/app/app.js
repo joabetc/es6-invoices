f497386a40a243c409557952ebd25054df65ce14
Joabe Costa <joabetc@hotmail.com>
2018-04-17 21:56:20 -0300

    Creating an EventEmitter

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,7 @@ import { log, timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { invoiceService as service } from './invoice/service.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
 const operations = pipe(
     partialize(takeUntil, 3),
@@ -10,7 +11,7 @@ const operations = pipe(
 
 const action = operations(() =>
     retry(3, 3000, () =>timeoutPromise(200, service.sumItems('2143')))
-        .then(console.log)
+        .then(sum => EventEmitter.emit('sumItems', sum))
         .catch(console.log)
 );
 
